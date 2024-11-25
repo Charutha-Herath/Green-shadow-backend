b3572552157aa4618575b6c14e89a4915d142424
Charutha-Herath <charuh2023@gmail.com>
2024-11-25 17:28:05 +0530

    add user dto

---
+++ src/main/java/lk/ijse/greenshadow/DTO/IMPL/UserDTO.java
@@ -0,0 +1,16 @@
+package lk.ijse.greenshadow.DTO.IMPL;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDTO {
+    @Id
+    private String email;
+    private String password;
+    private String role;
+}
