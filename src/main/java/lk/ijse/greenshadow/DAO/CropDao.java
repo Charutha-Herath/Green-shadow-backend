aad20b7323ef05849eaaa70d2881c943b4eed347
Charutha-Herath <charuh2023@gmail.com>
2024-11-25 17:47:40 +0530

    add crop dao

---
+++ src/main/java/lk/ijse/greenshadow/DAO/CropDao.java
@@ -0,0 +1,13 @@
+package lk.ijse.greenshadow.DAO;
+
+
+import lk.ijse.greenshadow.Entity.IMPL.CropEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CropDao extends JpaRepository<CropEntity,String> {
+    @Query(value = "SELECT * FROM crop WHERE crop_code = (SELECT crop_code FROM crop ORDER BY CAST(SUBSTRING(crop_code, 6) AS UNSIGNED) DESC LIMIT 1);", nativeQuery = true)
+    CropEntity findLastRowNative();
+}
