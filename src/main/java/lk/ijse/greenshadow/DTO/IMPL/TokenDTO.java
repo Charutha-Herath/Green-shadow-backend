9708f230ab56677a1cf7ae657459d5e4c1cf72ce
Charutha-Herath <charuh2023@gmail.com>
2024-12-02 16:29:47 +0530

    add token dto

---
+++ src/main/java/lk/ijse/greenshadow/DTO/IMPL/TokenDTO.java
@@ -0,0 +1,12 @@
+package lk.ijse.greenshadow.DTO.IMPL;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class TokenDTO {
+    private String token;
+}
