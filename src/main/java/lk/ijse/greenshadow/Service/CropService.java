36d48cf39c090cb5885c5429c11a6c601fed88e8
Charutha-Herath <charuh2023@gmail.com>
2024-12-06 00:09:33 +0530

    update crop classes

--- src/main/java/lk/ijse/greenshadow/Service/CropService.java
+++ src/main/java/lk/ijse/greenshadow/Service/CropService.java
@@ -1,15 +1,50 @@
-package lk.ijse.greenshadow.Service;
+package lk.ijse.greenshadow.service;
 
-
-
-import lk.ijse.greenshadow.DTO.IMPL.CropDTO;
+import lk.ijse.greenshadow.entity.Crop;
+import lk.ijse.greenshadow.repository.CropRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface CropService {
-    void saveCrop(CropDTO cropDTO);
-    List<CropDTO>getAllCrops();
-    CropDTO getCrop(String cropCode);
-    void deleteCrop(String cropCode);
-    void updateCrop(String cropCode, CropDTO cropDTO);
+@Service
+public class CropService {
+    @Autowired
+    private CropRepository cropRepository;
+
+    public List<Crop> getAllCrops(String sortBy) {
+        if (sortBy != null) {
+            return cropRepository.findAll(Sort.by(sortBy));
+        }
+        return cropRepository.findAll();
+    }
+
+    public Optional<Crop> getCropById(Long id) {
+        return cropRepository.findById(id);
+    }
+
+    public Crop createCrop(Crop crop) {
+        return cropRepository.save(crop);
+    }
+
+    public Crop updateCrop(Long id, Crop cropDetails) {
+        Crop crop = cropRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Crop not found"));
+
+        crop.setCropName(cropDetails.getCropName());
+        crop.setScientificName(cropDetails.getScientificName());
+        crop.setCropImage(cropDetails.getCropImage());
+        crop.setCategory(cropDetails.getCategory());
+        crop.setSeason(cropDetails.getSeason());
+        crop.setField(cropDetails.getField());
+
+        return cropRepository.save(crop);
+    }
+    public void deleteCrop(Long id) {
+        Crop crop = cropRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Crop not found"));
+        cropRepository.delete(crop);
+    }
 }
