113435615fab3a071edc1e594ce2bb3fc023382b
Charutha-Herath <charuh2023@gmail.com>
2024-12-06 00:45:02 +0530

    update log classes

---
+++ src/main/java/lk/ijse/greenshadow/repository/LogRepository.java
@@ -0,0 +1,7 @@
+package lk.ijse.greenshadow.repository;
+
+import lk.ijse.greenshadow.entity.Log;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LogRepository extends JpaRepository<Log, Long> {
+}
