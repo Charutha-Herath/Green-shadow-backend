0aeed1d8df4c1f65e6c402c26cda086155a17769
Charutha-Herath <charuh2023@gmail.com>
2024-12-06 01:41:09 +0530

    update jwt classes

---
+++ src/main/java/lk/ijse/greenshadow/dto/JwtResponse.java
@@ -0,0 +1,11 @@
+package lk.ijse.greenshadow.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+}
---
+++ src/main/java/lk/ijse/greenshadow/dto/MessageResponse.java
@@ -0,0 +1,10 @@
+package lk.ijse.greenshadow.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MessageResponse {
+    private String message;
+}
