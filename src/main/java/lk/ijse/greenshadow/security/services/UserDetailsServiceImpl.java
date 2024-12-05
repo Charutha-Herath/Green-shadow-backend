28e941fdae602f72ad3a2b89f19cda61984ddfc7
Charutha-Herath <charuh2023@gmail.com>
2024-12-06 01:30:00 +0530

    update user classes

---
+++ src/main/java/lk/ijse/greenshadow/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package lk.ijse.greenshadow.security.services;
+
+import lk.ijse.greenshadow.entity.User;
+import lk.ijse.greenshadow.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetailsImpl loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
