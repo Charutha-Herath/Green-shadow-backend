a03f51024fbde38554bb637bca298024046beb1c
Charutha-Herath <charuh2023@gmail.com>
2024-12-06 01:39:35 +0530

    update jwt classes

--- src/main/java/lk/ijse/greenshadow/Exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package lk.ijse.greenshadow.Exception;
-
-public class NotFoundException extends RuntimeException{
-    public NotFoundException(){}
-    public NotFoundException(String message){}
-    public NotFoundException(String message, Throwable cause){}
-}
