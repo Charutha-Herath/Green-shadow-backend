2754800bc78109c52ff755dfe7805d1ac459c9f0
Charutha-Herath <charuh2023@gmail.com>
2024-12-07 02:45:00 +0530

    update exception class

--- src/main/java/lk/ijse/greenshadow/exception/GlobalExceptionHandler.java
+++ src/main/java/lk/ijse/greenshadow/exception/GlobalExceptionHandler.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
